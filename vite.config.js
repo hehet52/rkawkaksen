16b3fc1de8e0963ce67554263cedc902843e0414
20251201-cpu <20251201@sungshin.ac.kr>
2026-08-06 13:36:17 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    /*
+     * true로 하면 localhost뿐 아니라
+     * 같은 와이파이의 다른 기기(팀원 컴퓨터)에서도
+     * 이 컴퓨터의 IP 주소로 접속할 수 있게 됩니다.
+     */
+    host: true,
+    port: 5173,
+  },
+});
